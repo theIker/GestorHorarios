8d4e1d1c295670e97c5e6694c81426fd883add35
2dam <2dam@MACINF01.TartangaLH.eus>
2018-02-02 09:13:49 +0100

    prueba iker branch

--- GestorHorariosApp/src/main/java/datos/dbmanager.java
+++ GestorHorariosApp/src/main/java/datos/dbmanager.java
@@ -39,7 +39,7 @@ public class dbmanager{
 	//////////////////////////////////////////////////
 	
         /**
-         * e
+         * eee
          * @param dni
          * @param pass
          * @return
